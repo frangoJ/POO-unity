cf41a99d63d2632fd27f802f86e444a7a058e44f
frangoJ <joaolucas142ajuda@gmail.com>
2025-05-13 11:37:12 -0300

    13.05.2025

--- POO/Assets/scripts/MovimentoInimigo.cs
+++ POO/Assets/scripts/MovimentoInimigo.cs
@@ -1,29 +1,60 @@
+using System;
+using System.Diagnostics;
 using UnityEngine;
 
 public class MovimentoInimigo : MonoBehaviour
 {
-    
+
     private GameObject player;
-    
+
     private Rigidbody rigidBody;
     private float velocidade;
 
     public float Raiovisao = 3;
+    private bool naVisao = false;
+
+    private SphereCollider _sphereCollider;
+
+    public float distanciaminima;
     void Start()
     {
         velocidade = gameObject.GetComponent<Inimigo>().Velocidade();
         rigidBody = GetComponent<Rigidbody>();
-        
+        _sphereCollider = gameObject.GetComponent<SphereCollider>();
         player = GameObject.FindWithTag("Player");
     }
 
-   
+
     void Update()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) < Raiovisao)
+        _sphereCollider.radius = Raiovisao;
+        
+        if (Vector3.Distance(transform.position, player.transform.position) > 2f)
+        if (naVisao == true)
         {
-            transform.LookAt(player.transform.position);
-            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, velocidade * Time.deltaTime);
+                transform.LookAt(player.transform.position);
+                transform.position = Vector3.MoveTowards(transform.position, player.transform.position,
+                    velocidade * Time.deltaTime);
+            
+           //  Debug.Drawline(transform.position, player.transform.position, Color.red);
+        }
+    }
+    
+    void OnTriggerStay(Collider colisao)
+    {
+        if (colisao.CompareTag("Player"))
+        {
+            naVisao = true;
+        }
+    }
+
+    void OnTriggerExit(Collider colisao)
+    {
+        if (colisao.CompareTag("Player"))
+        {
+            naVisao = false;
         }
     }
 }
+
+
